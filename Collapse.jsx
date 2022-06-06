0198b0798b2a3a58b7198d534026900377f54e18
Alexey Y <alexey.yashenkov@gmail.com>
2022-06-06 16:52:27 +0300

    Create Collapse.jsx

---
+++ Collapse.jsx
@@ -0,0 +1,47 @@
+export default class Collapse extends React.Component {
+  constructor(props) {
+    super(props);
+    const { opened } = props;
+    this.state = { opened };
+  }
+
+  handleToggle = (e) => {
+    e.preventDefault();
+    this.setState(({ opened }) => ({ opened: !opened }));
+  }
+
+  render() {
+    const { opened } = this.state;
+    const { text } = this.props;
+    const classes = cn({
+      collapse: true,
+      show: opened,
+    });
+
+    return (
+      <div>
+        <p>
+          <a
+            className="btn btn-primary"
+            onClick={this.handleToggle}
+            data-bs-toggle="collapse"
+            href="#"
+            role="button"
+            aria-expanded={opened}
+          >
+            Link with href
+          </a>
+        </p>
+        <div className={classes}>
+          <div className="card card-body">
+            {text}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Collapse.defaultProps = {
+  opened: true,
+};
