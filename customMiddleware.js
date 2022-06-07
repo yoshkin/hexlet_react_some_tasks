f938f4108c09aab75074b6a167923e13a90ac4b6
Alexey Y <alexey.yashenkov@gmail.com>
2022-06-07 17:47:58 +0300

    Create customMiddleware.js

---
+++ customMiddleware.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console, no-unused-vars */
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+const addDate = (store) => (next) => (action) => {
+  // BEGIN (write your solution here)
+  const result = next(action);
+  if (action.type !== 'TASK_ADD') {
+    return result;
+  }
+  const today = new Date();
+  const formatedDate = today.toLocaleDateString('ru-RU');
+  result.payload.task.text = `Задача на ${formatedDate}: ${result.payload.task.text}`;
+  return result;
+  // END
+};
+
+export default { logger, addDate };
+
